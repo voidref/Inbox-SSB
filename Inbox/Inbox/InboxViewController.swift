59e7220fc794eb6de410a3ef1eb5254447f90d87
Alan Westbrook <alan@rockwoodsoftware.com>
2015-03-17 11:10:00 -0700

    Set user agent
    * Add VC and set the user agent on the web view when loading

---
+++ Inbox/Inbox/InboxViewController.swift
@@ -0,0 +1,23 @@
+//
+//  InboxViewController.swift
+//  Inbox
+//
+//  Created by Alan Westbrook on 3/17/15.
+//  Copyright (c) 2015 Marcelo Marfil. All rights reserved.
+//
+
+import Cocoa
+import WebKit
+
+class InboxViewController: NSViewController {
+    @IBOutlet var webView:WebView? = nil
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+        
+        webView?.customUserAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_3) AppleWebKit/600.5.9 (KHTML, like Gecko) Version/8.0.5 Safari/600.5.9"
+        
+    }
+    
+}
