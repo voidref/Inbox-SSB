4f2fe21159ace30191f6af6d0ddced837ab2328c
Alan Westbrook <alan@rockwoodsoftware.com>
2015-03-17 13:12:54 -0700

    ULAY HAC
    * Create a temp web view attached to the current window in order to
    just open a link in an external browser

--- Inbox/Inbox/InboxViewController.swift
+++ Inbox/Inbox/InboxViewController.swift
@@ -18,7 +18,29 @@ class InboxViewController: NSViewController {
         
         webView?.customUserAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_3) AppleWebKit/600.5.9 (KHTML, like Gecko) Version/8.0.5 Safari/600.5.9"
         webView?.mainFrameURL = "http://inbox.google.com/"
-        
+        webView?.policyDelegate = self
+        webView?.UIDelegate = self
     }
     
+    override func webView(webView: WebView!, decidePolicyForNavigationAction actionInformation: [NSObject : AnyObject]!, request: NSURLRequest!, frame: WebFrame!, decisionListener listener: WebPolicyDecisionListener!) {
+        if let hostActual = request.URL?.host {
+            println(hostActual)
+            if hostActual.hasSuffix("google.com") {
+                listener.use()
+            }
+            else {
+                listener.ignore()
+                webView.removeFromSuperview()
+                NSWorkspace.sharedWorkspace().openURL(request.URL!)
+            }
+        }
+    }
+    
+    override func webView(sender: WebView!, createWebViewWithRequest request: NSURLRequest!) -> WebView! {
+        let haxview = WebView(frame: NSMakeRect(0, 0, 10, 10))
+        haxview.policyDelegate = self
+        sender.superview!.addSubview(haxview)
+        return haxview
+    }
 }
+
